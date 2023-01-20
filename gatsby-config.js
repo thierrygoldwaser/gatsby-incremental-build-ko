f80e9ca528d3b6c9fd2569b0a0764b15ff618b89
Thierry Goldwaser <thierry@codarium.io>
2023-01-20 21:53:51 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-incremental-build-ko`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
